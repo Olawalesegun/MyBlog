ff3cd550414fc12a5e367b6cac38109bc95fe93b
Olawalesegun <olawalesegun0@gmail.com>
2023-07-29 22:35:14 +0100

    users ---

---
+++ src/main/java/data/models/User.java
@@ -0,0 +1,20 @@
+package data.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Entity(name = "users")
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+
+}
