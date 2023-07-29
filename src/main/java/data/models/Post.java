24f1822767b5e7e6985b08540784069a4e5ea59c
Olawalesegun <olawalesegun0@gmail.com>
2023-07-29 23:29:21 +0100

    Post __-_--

--- src/main/java/data/models/Post.java
+++ src/main/java/data/models/Post.java
@@ -1,2 +1,21 @@
-package data.models;public class Post {
+package data.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "posts")
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long postId;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String body;
+    @Column(nullable = false)
+    private long userId;
 }
