1b7a12b357099bbdf288098044565f53e60b23d8
Olawalesegun <olawalesegun0@gmail.com>
2023-07-29 23:29:42 +0100

    User ----

--- src/main/java/data/models/User.java
+++ src/main/java/data/models/User.java
@@ -3,13 +3,14 @@ package data.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
 @Entity(name = "users")
 @Setter
 @Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private long userId;
     @Column(unique = true, nullable = false)
     private String email;
     @Column(nullable = false)
